10d5b377610482222e55d52d29ea542d93da6709
vulture20 <vulture20@web.de>
2015-01-15 10:44:07 +0100

    Update HeatControl.ino

--- HeatControl.ino
+++ HeatControl.ino
@@ -12,20 +12,27 @@
 #include <EEPROMex.h>
 #include <EEPROMVar.h>
 #include <tmp102.h>
+#include "HeatControl.h"
 
 const byte sensorAddress = 0x90;           // I2C address of the tmp102 sensor
 const unsigned long tempInterval = 15000;  // read temperature only every x ms
 const int pwmPin = 9;                      // Pin for PWM output to heating
+const int xbeeRXPin = 10;                  // RX Pin for the xbee
+const int xbeeTXPin = 11;                  // TX Pin for the xbee
 
-SoftwareSerial xbee(10, 11); // RX, TX
+SoftwareSerial xbee(xbeeRXPin, xbeeTXPin); // Pins for the serial xbee connection
 int cmdRecvd = 0;            // is received Command complete?
 String cmd = "";             // received Command
 float temperature = 0.0;     // Buffer for the last read temperature
 unsigned long tempLast;      // last time the temp was read
+sDOWSettings DOWSettings;
 
 tmp102 *thermometer = new tmp102(&Wire);
 
 void setup() {
+  pinMode(pwmPin, OUTPUT);
+  analogWrite(pwmPin, 0);
+  
   Serial.begin(57600); // for debugging
   xbee.begin(9600); // Uplink to server over xbee
   cmd.reserve(30); // reserve 30 bytes for the command string
@@ -87,4 +94,33 @@ void setRTC(String timeStr) {
       setTime(pctime);
     }
   }
+}
+
+// is the Holiday mode active?
+boolean isHolidayActive() {
+  return (EEPROM.readByte(122) == 1);
+}
+
+// set the holiday mode
+boolean setHolidayMode(boolean mode) {
+  return (EEPROM.updateByte(122, mode));
+}
+
+// set the pwm output to x percent (mapped to 10 bit)
+void setPWM(byte percent) {
+  analogWrite(pwmPin, map(percent, 0, 100, 0, 1023));
+}
+
+// read settings for the given day of the week from eeprom
+void getDOWSettings(byte DOW) {
+  DOWSettings.set1BeginHour =   EEPROM.readByte (((DOW - 1) * 16 + 2));
+  DOWSettings.set1BeginMinute = EEPROM.readByte (((DOW - 1) * 16 + 2) + 1);
+  DOWSettings.set1EndHour =     EEPROM.readByte (((DOW - 1) * 16 + 2) + 2);
+  DOWSettings.set1EndMinute =   EEPROM.readByte (((DOW - 1) * 16 + 2) + 3);
+  DOWSettings.set1Temp =        EEPROM.readFloat(((DOW - 1) * 16 + 2) + 4);
+  DOWSettings.set2BeginHour =   EEPROM.readByte (((DOW - 1) * 16 + 2) + 8);
+  DOWSettings.set2BeginMinute = EEPROM.readByte (((DOW - 1) * 16 + 2) + 9);
+  DOWSettings.set2EndHour =     EEPROM.readByte (((DOW - 1) * 16 + 2) + 10);
+  DOWSettings.set2EndMinute =   EEPROM.readByte (((DOW - 1) * 16 + 2) + 11);
+  DOWSettings.set2Temp =        EEPROM.readFloat(((DOW - 1) * 16 + 2) + 12);
 }
