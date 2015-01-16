ecea4735a6ba0bb9a6b350cbd5a7ba065875ba0b
vulture20 <vulture20@web.de>
2015-01-16 12:16:05 +0100

    Update HeatControl.h

--- HeatControl.h
+++ HeatControl.h
@@ -1,6 +1,8 @@
 #ifndef HeatControl_h
 #define HeatControl_h
 
+#define minValue(a, b) ((a < b) ? a : b)
+
 struct sDOWSettings {
   byte set1BeginHour;
   byte set1BeginMinute;
@@ -12,6 +14,14 @@ struct sDOWSettings {
   byte set2EndHour;
   byte set2EndMinute;
   float set2Temp;
+};
+
+namespace State {
+  typedef enum SysteMode_e {
+    None     = 0,
+    Default  = (1<<0),
+    Settings = (1<<1)
+  } SystemMode;
 };
 
 #endif /* HeatControl_h */
